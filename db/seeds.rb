e3884cd8362e5606cd4e95debc23cd343d0dd3df
Jisu Kim <jisu.kim@flatironschool.com>
2014-09-18 14:05:35 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+App.destroy_all
+Tag.destroy_all
+
+rails = Tag.create!(name: 'Rails')
+angularjs = Tag.create!(name: 'AngularJS')
+javascript = Tag.create!(name: 'Javascript')
+jquery = Tag.create!(name: 'jQuery')
+
+apps = App.create([
+  {name: 'Happy-Hr', description: 'list of all the happy hour drinks...', url: 'www.happy-hr.herokuapp.com', tags: [angularjs, rails]},
+  {name: 'Language8020', description: 'language learning application', url: 'www.language-80-20.herokuapp.com', tags: [rails, javascript]},
+  {name: 'Ebates', description: 'GPT site', url: 'www.ebates.herokuapp.com', tags: [rails]},
+  {name: 'Good Read', description: 'list of favorite books', url: 'www.goodread.herokuapp.com', tags: [rails, javascript, jquery]}
+])
+
+puts "#{App.count} Apps created"
