fb5a6e3e8c2948aa3b17704ff67beb973a4a6174
Jisu Kim <jisu.kim@flatironschool.com>
2014-09-19 16:57:28 -0400

    Include default of showing all

--- app/models/app.rb
+++ app/models/app.rb
@@ -2,5 +2,5 @@ class App < ActiveRecord::Base
   has_many :app_tags
   has_many :tags, through: :app_tags
 
-  scope :filter, ->(name){joins(:tags).where('tags.name = ?', name) if name.present?}
+  scope :filter, ->(name){joins(:tags).where('tags.name = ?', name) if name.present? && name != 'Show All'}
 end
