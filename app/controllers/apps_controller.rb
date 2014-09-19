b51d4a915b9ba503ce9d89e6e7232646dd5777c4
Jisu Kim <jisu.kim@flatironschool.com>
2014-09-19 16:56:40 -0400

    Improve SQL query using includes

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,6 +1,6 @@
 class AppsController < ApplicationController
   def index
-    @apps = App.filter(params[:filter])
+    @apps = App.includes(:tags).filter(params[:filter])
     @tags = Tag.all
   end
 end
