b0ad2cf5e99d37ab15e8f3e9b7dc7a5019a8f789
Jisu Kim <jisu.kim@flatironschool.com>
2014-09-22 16:17:24 -0400

    Add 'showall' id and add styling

--- app/models/app.rb
+++ app/models/app.rb
@@ -3,4 +3,5 @@ class App < ActiveRecord::Base
   has_many :tags, through: :app_tags
 
   scope :filter, ->(name){joins(:tags).where('tags.name = ?', name) if name.present? && name != 'Show All'}
+
 end
