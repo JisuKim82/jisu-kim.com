3feeadf17e6d8f45a474f3d8a989dad3323b6496
Jisu Kim <jisu.kim@flatironschool.com>
2014-09-29 22:38:05 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,13 +18,13 @@ rspec = Tag.create!(name: 'RSpec')
 show_all = Tag.create!(name: 'Show All')
 
 apps = App.create([
-  {name: 'Language8020', description: 'Language learning application. Based on the premise that 80% of a person\'s daily conversation is derived from 20% of a language\'s vocabulary.', url: 'language-80-20.herokuapp.com', tags: [rails, javascript, jquery, rspec]},
+  {name: 'Language8020', description: 'Language learning application. Based on the premise that 80% of a person\'s daily conversation is derived from 20% of one\'s vocabulary.', url: 'language-80-20.herokuapp.com', tags: [rails, javascript, jquery, rspec]},
   {name: 'Ebates', description: 'Get Paid To site. Created an affiliated site to direct traffic to ebates.com and used nokogiri to scrape data of the cashback offered by retailers.', url: 'ebates.herokuapp.com', tags: [rails, jquery, javascript, nokogiri]},
   {name: 'Good Read', description: 'List of recommended books. Ability to add/edit favorite books and generates the bookcover based on the ISBN. Implemented a search engine that makes the data searchable based on title, author or description. Records also get persisted to the database.', url: 'goodread.herokuapp.com', tags: [rails, javascript, jquery, rspec]},
   {name: 'Happy-Hr', description: 'List of popular drinks and reviews. Utilized the popular frontend framework AngularJS for the two way data binding.', url: 'happy-hr.herokuapp.com', tags: [angularjs, javascript]},
-  {name: 'Poker Sharks', description: 'Bio\'s of the professional poker players. Utilized AngularJS for the two way data binding and created custom directives', url: 'poker-sharks.herokuapp.com', tags: [angularjs, javascript, jquery]},
+  {name: 'Poker Sharks', description: 'Bio\'s of the professional poker players. Utilized AngularJS for the two way data binding and created custom directives.', url: 'poker-sharks.herokuapp.com', tags: [angularjs, javascript, jquery]},
   {name: 'Device Sniffer', description: 'Application that collects the MAC addresses from the Meraki router setup at the Flatiron School and determines the manufacturers of the devices. Provides real-time data of the devices and displays the data visually using a dashboard.', url: 'devicesniffer.herokuapp.com', tags: [rails, javascript, rspec]},
-  {name: 'Rails jQuery', description: 'Applied jQuery techniques learned such as event handlers', url: 'rails-jquery.herokuapp.com', tags: [rails, jquery]},
+  {name: 'Rails jQuery', description: 'Applied jQuery techniques learned such as event handlers.', url: 'rails-jquery.herokuapp.com', tags: [rails, jquery]},
   {name: 'Vacant Lots', description: 'Parsed vacant lot data from NYC Open Data to find possible locations for community gardents. Utilized Google\'s street view API to show lots with the most potential.', url: 'vacant-lots.herokuapp.com', tags: [rails, javascript, jquery]}
 ])
 
